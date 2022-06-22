b2fe7e46b8a4877cc86ff9430368af5b1678a3e7
dans300 <danielsierramejia@gmail.com>
2022-06-21 21:35:59 -0500

    anadimmos funcionalidades a los heroes como crud ,simulamos un servidor con un api tambien

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -11,8 +11,21 @@ import { MessageService } from '../message.service';
 export class HeroesComponent implements OnInit {
  
   heroes:Hero[]=[];
+
+
+  delete(hero: Hero): void {
+    this.heroes = this.heroes.filter(h => h !== hero);
+    this.heroService.deleteHero(hero.id).subscribe();
+  }
   
-  
+  add(name: string): void {
+    name = name.trim();
+    if (!name) { return; }
+    this.heroService.addHero({ name } as Hero)
+      .subscribe(hero => {
+        this.heroes.push(hero);
+      });
+  }
 
   getHeroes(): void {
     this.heroService.getHeroes().subscribe(heroes=>this.heroes=heroes);
